3c44af00aea5c651c4a7abbed369fa24aa4a1b93
alessandrovargiu <alessandrovargiu98@gmail.com>
2023-05-14 13:25:26 +0200

    minor changes

--- cap4/FSM/myTypes.vhd
+++ cap4/FSM/myTypes.vhd
@@ -7,25 +7,20 @@ package myTypes is
     constant OP_CODE_SIZE : integer :=  6;                                              -- OPCODE field size
     constant FUNC_SIZE    : integer :=  11;                                             -- FUNC field size
 
--- R-Type instruction -> FUNC field
-    constant RTYPE_ADD : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";    -- ADD RS1,RS2,RD
-    constant RTYPE_SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000001";    -- SUB RS1,RS2,RD
+    --******************************* FUNC bits ****************************************--
+    constant RTYPE_ADD : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";    
+    constant RTYPE_SUB : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000001";    
     constant RTYPE_AND : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000010";
     constant RTYPE_OR : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000011";
-    -- ...................
-    -- to be completed with the others 2 alu operation
-    -- ...................
-   -- constant NOP : std_logic_vector(FUNC_SIZE - 1 downto 0) :=  "00000000000";
-    constant NOP : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000010"; 
+    
+    --**************************** OPCODEs ***************************************    
 
 -- R-Type instruction -> OPCODE field
     constant RTYPE : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000000";          -- for ADD, SUB, AND, OR register-to-register operation
+    constant NOP : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000010";
 
 -- I-Type instruction -> OPCODE field
-    constant ITYPE_ADDI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000001";    -- ADDI1 RS1,RD,INP1
-    -- ...................
-    -- to be completed with the others I-Type instructions
-    -- ...................
+    constant ITYPE_ADDI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000001";    
     constant ITYPE_SUBI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000011";
     constant ITYPE_ANDI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000100";
     constant ITYPE_ORI1 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "000101";
@@ -41,7 +36,6 @@ package myTypes is
     constant ITYPE_L_MEM2 : std_logic_vector(OP_CODE_SIZE - 1 downto 0) :=  "001111";
 
 
--- Change the values of the instructions coding as you want, depending also on the type of control unit choosen
 
 end myTypes;
 
